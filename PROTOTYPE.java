60554b17d1f23e6328a5723fd0ebca1c72b53b58
ddim7664@student.anu.ac.kr <ddim7664@gmail.com>
2022-04-18 00:08:45 +0900

    Initial commit

---
+++ PROTOTYPE.java
@@ -0,0 +1,13 @@
+
+
+
+
+class Square extends React.Component {
+    render() {
+        return (
+                <button className="square" onClick={function() { console.log('click'); }}>
+        {this.props.value}
+      </button>
+    );
+    }
+}
